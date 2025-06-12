de3985cbfff11dd5cb6cfa973b0e55e6590d5767
Rafael Abednego <rafaelabednego123@gmail.com>
2025-06-13 01:50:43 +0700

    Remove debug method

--- app/src/main/java/com/rafael0112/asessment3/network/WikulApiService.kt
+++ app/src/main/java/com/rafael0112/asessment3/network/WikulApiService.kt
@@ -5,9 +5,7 @@ import com.rafael0112.asessment3.model.WikulStatus
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.MultipartBody
-import okhttp3.OkHttpClient
 import okhttp3.RequestBody
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.DELETE
@@ -22,14 +20,6 @@ import retrofit2.http.Path
 
 private const val BASE_URL = "https://551d-35-229-190-161.ngrok-free.app/api/"
 
-private val logging = HttpLoggingInterceptor().apply {
-    level = HttpLoggingInterceptor.Level.BODY
-}
-
-private val client = OkHttpClient.Builder()
-    .addInterceptor(logging)
-    .build()
-
 private val moshi = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
@@ -37,7 +27,6 @@ private val moshi = Moshi.Builder()
 private val retrofit = Retrofit.Builder()
     .addConverterFactory(MoshiConverterFactory.create(moshi))
     .baseUrl(BASE_URL)
-    .client(client)
     .build()
 
 interface WikulApiService {
