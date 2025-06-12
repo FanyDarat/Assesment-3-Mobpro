4134a38ebe499b2ad459a5e72899cb85542f1fbb
Rafael Abednego <rafaelabednego123@gmail.com>
2025-06-12 19:40:55 +0700

    Memperbaiki nama aplikasi

--- app/src/androidTest/java/com/rafael0112/asessment3/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/rafael0112/asessment3/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.rafael0112.mobpro1", appContext.packageName)
+        assertEquals("com.rafael0112.asessment3", appContext.packageName)
     }
 }
