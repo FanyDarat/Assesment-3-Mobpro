09fa06c679486697bcf3f64119f5d0dcabe38d0f
Rafael Abednego <rafaelabednego123@gmail.com>
2025-06-12 18:49:38 +0700

    Membuat model untuk menerima response

--- app/src/main/java/com/rafael0112/asessment3/model/OpStatus.kt
+++ app/src/main/java/com/rafael0112/asessment3/model/OpStatus.kt
@@ -2,5 +2,6 @@ package com.rafael0112.asessment3.model
 
 data class OpStatus(
     var success: String,
-    var message: String?
+    var message: String?,
+    var data: String?
 )
--- app/src/main/java/com/rafael0112/asessment3/model/WikulStatus.kt
+++ app/src/main/java/com/rafael0112/asessment3/model/WikulStatus.kt
@@ -3,5 +3,5 @@ package com.rafael0112.asessment3.model
 data class WikulStatus(
     val success: Boolean,
     val message: String,
-    val data: List<Wikul>
+    val data: List<Wikul> 
 )
